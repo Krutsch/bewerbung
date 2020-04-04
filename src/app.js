11d2a48df7ce1b046ea5ee356326a2bb1bbc82f8
Fabian Krutsch <f.krutsch@gmx.de>
2020-04-04 23:22:45 +0100

    Update CV

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 new IntersectionObserver(
-  function(entries) {
+  function (entries) {
     entries[0].isIntersecting
       ? document.body.classList.add("zoom")
       : document.body.classList.remove("zoom");
   },
   {
     rootMargin: "50px",
-    threshold: 0
+    threshold: 0,
   }
 ).observe(document.querySelector("footer"));
