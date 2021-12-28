697cff1b0478794b0fa63c08d7754515bbd23457
Fabian Krutsch <fabsen.k27@gmail.com>
2021-12-28 11:44:03 +0100

    update

--- utils/prebuild.js
+++ utils/prebuild.js
@@ -1,53 +1,33 @@
 import fs from "fs";
-import glob from "glob";
 import sharp from "sharp";
 
 const SOURCE_FOLDER = "src";
 const BUILD_FOLDER = "build";
+const file = process.argv[2];
 
-// Remove old build folder
-fs.rmSync(BUILD_FOLDER, { recursive: true, force: true });
-
-copyFiles(["src/_headers"]);
-glob("src/**/*.!(js|ts|html|css)", {}, (err, files) => {
-  if (err) throw err;
-
-  copyFiles(files.filter((f) => /\.(woff|txt|ico)/.test(f)));
-  jpegHandler(files.filter((f) => f.endsWith(".jpg")));
-  webpHandler(files.filter((f) => f.endsWith(".webp")));
-
-  console.log(`🛠️  Pre-build finished.`);
-});
-
-function copyFiles(files) {
-  files.forEach(copyFile);
+if (file === "src/_headers" || /\.(woff|txt|ico)/.test(file)) {
+  copyFile(file);
+} else if (file.endsWith(".jpg")) {
+  sharp(file)
+    .jpeg({
+      quality: 80,
+    })
+    .toFile(toBuildFile(file))
+    .catch((err) => {
+      console.error(err);
+    });
+} else if (file.endsWith(".webp")) {
+  sharp(file)
+    .webp({
+      quality: 80,
+    })
+    .toFile(toBuildFile(file))
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
-function jpegHandler(files) {
-  files.forEach((file) => {
-    sharp(file)
-      .jpeg({
-        quality: 80,
-      })
-      .toFile(toBuildFile(file))
-      .catch((err) => {
-        console.error(err);
-      });
-  });
-}
-
-function webpHandler(files) {
-  files.forEach((file) => {
-    sharp(file)
-      .webp({
-        quality: 80,
-      })
-      .toFile(toBuildFile(file))
-      .catch((err) => {
-        console.error(err);
-      });
-  });
-}
+console.log(`finished handling ${file}`);
 
 function copyFile(file) {
   const buildFile = toBuildFile(file);
